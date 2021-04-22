da716a48ff758706b3f68a694af85b7d2cabf9e3
Alpha Vylly <65187737+AlphaLawless@users.noreply.github.com>
2021-04-22 16:28:59 -0300

    Updage in Slug

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,13 +1,15 @@
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
-import { GetStaticPaths, GetStaticProps } from 'next'
 
 import { api } from '../../services/api'
 import { convertDurationToTimeString } from '../../utils/ConvertDurationToTimeString'
 
 import styles from './episode.module.scss'
+
+
 
 
 type Episode = {
@@ -62,11 +64,32 @@ export default function Episode({ episode }: EpisodeProps) {
   )
 }
 //Slug serve para deixar o nome bonita na URL, atrativa ao olhos de quem ver.
+//Rotas que tem cochetes, precisam usar o método GetStaticPaths() para passarem os caminhos de forma dinamica.
 export const getStaticPaths: GetStaticPaths = async () => {
+  //Passando a request da homme para a pasta de episodios
+  const { data } = await api.get('episodes', {
+    //Passando os parametros em formato estruturado pelo JavaScript.
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.mat(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
+  //OBS: com essa variavel paths, passando pelo data, os dois últimos episódios vão ser gerados de forma static.
+  //O método fallback pode ser definido como: incremental static regeneration
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
