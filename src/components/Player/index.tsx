c90e0ab89c5dd6aa426d92938c1afa3d64437348
Alpha Vylly <65187737+AlphaLawless@users.noreply.github.com>
2021-04-22 17:49:03 -0300

    Create tag audio in Application

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -57,20 +57,27 @@ export default function Player() {
           <span>00:00</span>
         </div>
 
+        {episode && (
+          <audio
+            src={episode.url}
+            autoPlay
+          />
+        )}
+
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.playButton}>
+          <button type="button" className={styles.playButton} disabled={!episode}>
             <img src="/play.svg" alt="Tocar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
